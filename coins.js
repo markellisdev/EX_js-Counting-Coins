6f0b500e9373fea95363616a56fe59d2e3e900f4
Mark Ellis <markellisdev@gmail.com>
2016-10-18 13:08:13 -0500

    Complete

--- coins.js
+++ coins.js
@@ -1,66 +1,4 @@
 
-// First method attempt ----------------
-// var numInput = prompt("Please enter an amount, in cents, you'd like converted into coins.<br>Example: 0.63");
-
-// var amount = parseFloat(numInput);
-
-// console.log("amount ,", amount, typeof(amount));
-
-// var penny = 0.01;
-// var nickel = 0.05;
-// var dime = 0.10;
-// var quarter = 0.25;
-
-// var numOfQuarters;
-// var numOfDimes;
-// var numOfNickels;
-// var numOfPennies;
-
-// console.log("amount = ", Number(amount));
-
-// console.log("quarter type ,", typeof(quarter));
-
-
-
-// if (amount > quarter) {
-  // var numOfQuarters = amount / quarter;
-  // console.log("numOfQuarters", Math.floor(numOfQuarters));
-  // minusQuarters = parseFloat(amount - (quarter * Math.floor(numOfQuarters)));
-  // console.log("minusQuarters = ", minusQuarters, typeof(minusQuarters));
-// }
-
-// if (minusQuarters > dime) {
-  // var numOfDimes = minusQuarters / dime;
-  // console.log("numOfDimes", Math.floor(numOfDimes));
-  // minusDimes = parseFloat(minusQuarters - (dime * Math.floor(numOfDimes)));
-// }
-
-
-// console.log("minusDimes = ", minusDimes, typeof(minusDimes));
-
-// if (minusDimes > nickel) {
-  // var numOfNickels = minusDimes / nickel;
-  // console.log("numOfNickels", Math.floor(numOfNickels));
-  // minusNickels = parseFloat(minusDimes - (nickel * Math.floor(numOfNickels)));
-// }
-
-// console.log("minusDimes = ", minusDimes, typeof(minusDimes));
-
-// else if (amount >= dime ) {
-//   var numOfDimes = amount /= dime;
-// }
-
-// else if (amount >= nickel) {
-//   var numOfNickels = amount /= nickel;
-// }
-
-// else if(amount > penny) {
-//   var numOfPennies = amount /= penny;
-// }
-
-//First method attempt END --------------
-
-
 /*
   Input: 0.67
   Output:
@@ -79,40 +17,70 @@ var amount = parseFloat(numInput);
 
 console.log("amount ,", amount, typeof(amount));
 
+var coinPurse = {
+  numOfQuarters:0, numOfDimes:0, numOfNickels:0, numOfPennies:0
+}
+
 var penny = 0.01;
 var nickel = 0.05;
 var dime = 0.10;
 var quarter = 0.25;
 
-  var numOfQuarters = amount / quarter;
-  console.log("numOfQuarters", Math.floor(numOfQuarters));
-  minusQuarters = parseFloat(amount - (quarter * Math.floor(numOfQuarters)));
+function coinCounter () {
+
+
+  coinPurse.numOfQuarters = Math.floor(amount / quarter);
+  console.log("numOfQuarters", Math.floor(coinPurse.numOfQuarters));
+  minusQuarters = parseFloat(amount - (quarter * Math.floor(coinPurse.numOfQuarters)));
   console.log("minusQuarters = ", minusQuarters, typeof(minusQuarters));
 
-  var numOfDimes = minusQuarters / dime;
-  console.log("numOfDimes", Math.floor(numOfDimes));
-  minusDimes = parseFloat(minusQuarters - (dime * Math.floor(numOfDimes)));
+  coinPurse.numOfDimes = Math.floor(minusQuarters / dime);
+  console.log("coinPurse.numOfDimes", Math.floor(coinPurse.numOfDimes));
+  minusDimes = parseFloat(minusQuarters - (dime * Math.floor(coinPurse.numOfDimes)));
 
-  var numOfNickels = minusDimes / nickel;
-  console.log("numOfNickels", Math.floor(numOfNickels));
-  minusNickels = parseFloat(minusDimes - (nickel * Math.floor(numOfNickels)));
+  coinPurse.numOfNickels = Math.floor(minusDimes / nickel);
+  console.log("coinPurse.numOfNickels", Math.floor(coinPurse.numOfNickels));
+  minusNickels = parseFloat(minusDimes - (nickel * Math.floor(coinPurse.numOfNickels)));
 
-  var numOfPennies = minusNickels / penny;
-  console.log("numOfPennies", Math.floor(numOfPennies));
-  minusPennies = parseFloat(minusNickels - (penny * Math.floor(numOfPennies)));
+  coinPurse.numOfPennies = Math.floor(minusNickels / penny);
+  console.log("coinPurse.numOfPennies", Math.floor(coinPurse.numOfPennies));
+  minusPennies = parseFloat(minusNickels - (penny * Math.floor(coinPurse.numOfPennies)));
 
-
-
-
-
-
-
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  var coinPurse = [numOfQuarters, numOfDimes, numOfNickels, numOfPennies]
-
-  return coinPurse;
+  document.getElementById("coinCounter").innerHTML = ("To get the amount you entered, $" + amount + ",<br><br>you'll need " + coinPurse.numOfQuarters + " quarter(s),<br>" + coinPurse.numOfDimes + " dime(s),<br>" + coinPurse.numOfNickels + " nickel(s),<br>" + coinPurse.numOfPennies + " pennies.");
 }
 
-var coins = coinCounter()
-console.log();
+coinCounter();
+
+
+
+
+
+
+// function coinCounter () {
+//   // Initialize a JavaScript object to hold the coins
+//   var coinPurse = [numOfQuarters, numOfDimes, numOfNickels, numOfPennies]
+
+//     document.getElementById("coinCounter").innerHTML = numOfQuarters[0];
+//   return coinPurse;
+// }
+
+// var coins = coinCounter()
+console.log("coinPurse", coinPurse);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
